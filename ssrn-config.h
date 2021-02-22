62178be2bc8a3c759e42f646008e2da2a7815502
ted yapo <ted.yapo@gmail.com>
2021-02-22 17:34:54 -0500

    added ssrn-config.h shim

---
+++ ssrn-config.h
@@ -0,0 +1,6 @@
+#ifndef ARDUINO_SRN_CONFIG_H_INCLUDED
+#define ARDUINO_SRN_CONFIG_H_INCLUDED
+
+#include "src/serial-ring-network/ssrn-config/Arduino-AltSoftSerial/ssrn-config.h"
+
+#endif //#ifndef ARDUINO_SRN_CONFIG_H_INCLUDED
