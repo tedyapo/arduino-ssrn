54dadb6b24e9faa501f296887e35478143c2976a
ted yapo <ted.yapo@gmail.com>
2021-02-24 21:57:48 -0500

    removed old inactive dir

--- ssrn/ssrn.cpp
+++
@@ -1,3 +0,0 @@
-// shim to compile ssrn base code as C++ for arduino 
-// so we can interact with serial objects
-#include "src/ssrn/serial-ring-network/ssrn/ssrn.c"
--- ssrn/ssrn.h
+++
@@ -1,8 +0,0 @@
-#ifndef ARDUINO_SRN_H_INCLUDED
-#define ARDUINO_SRN_H_INCLUDED
-
-#include "src/ssrn/serial-ring-network/ssrn-config/Arduino-AltSoftSerial/ssrn-config.h"
-#include "src/ssrn/serial-ring-network/ssrn/ssrn.h"
-
-
-#endif //#ifndef ARDUINO_SRN_H_INCLUDED
